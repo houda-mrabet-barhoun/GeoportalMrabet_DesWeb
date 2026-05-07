d059aeadb6427c8f1e78b210d5a04e12d441d857
houda-mrabet-barhoun <hod.mrabet@gmail.com>
2026-05-07 16:59:46 +0200

    Crer carpetas personalizadas

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
+import { MenuComponent } from './components/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonToggleModule],
+  imports: [RouterOutlet, MatButtonToggleModule, MenuComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- web/src/app/components/menu/menu.component.ts
+++ web/src/app/components/menu/menu.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+ 
+  showMessage(){
 
+  }
 }
