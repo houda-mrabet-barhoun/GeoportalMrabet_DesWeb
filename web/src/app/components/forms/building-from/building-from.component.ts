2001b8170239c558a9d717f1b4272f05fa9dfd32
houda-mrabet-barhoun <hod.mrabet@gmail.com>
2026-05-07 16:16:49 +0200

    Crer carpetas de componentes

---
+++ web/src/app/components/forms/building-from/building-from.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-building-from',
+  standalone: true,
+  imports: [],
+  templateUrl: './building-from.component.html',
+  styleUrl: './building-from.component.scss'
+})
+export class BuildingFromComponent {
+
+}
