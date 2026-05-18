e80c175c9a33a8882b434f73bbd4a5cb59f358ca
houda-mrabet-barhoun <hod.mrabet@gmail.com>
2026-05-18 17:33:41 +0200

    Update geoportal project

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HelpComponent } from './components/help/help.component';
 import { MapComponent } from './components/map/map.component';
+import { AboutComponent } from './components/about/about.component';
 
 import { RutasFormComponent } from './components/forms/rutas-form/rutas-form.component';
 import { ClientesFormComponent } from './components/forms/clientes-form/clientes-form.component';
@@ -13,6 +14,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'help', component: HelpComponent },
   { path: 'map', component: MapComponent },
+  { path: 'about', component: AboutComponent },
 
   { path: 'rutas-form', component: RutasFormComponent },
   { path: 'clientes-form', component: ClientesFormComponent },
